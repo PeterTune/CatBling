662d721d146665a228329853caf62c9cf3e521f8
JoseMartinCZ <74085376+JoseMartinCZ@users.noreply.github.com>
2024-06-12 00:17:35 -0400

    base

---
+++ CatBling/Assets/Contador.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using TMPro;
+
+public class CounterTMPro : MonoBehaviour
+{
+    public TMP_Text counterText; // Referencia al componente de texto TextMesh Pro en la UI
+    private int counter = 0; // Variable para llevar el conteo
+
+    void Start()
+    {
+        // Inicializa el texto con el valor del contador
+        UpdateCounterText();
+    }
+
+    void Update()
+    {
+        // Verifica si se presiona cualquier tecla
+        if (Input.anyKeyDown)
+        {
+            // Incrementa el contador
+            counter++;
+            // Actualiza el texto del contador
+            UpdateCounterText();
+        }
+    }
+
+    void UpdateCounterText()
+    {
+        // Actualiza el texto del componente de UI con el valor actual del contador
+        counterText.text = counter.ToString();
+    }
+}
