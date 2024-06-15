7ab765da355381d367bc99ea2a109c50fb27e219
Joaquín Pabst <jpabstolmedo@gmail.com>
2024-06-15 15:57:17 -0400

    botones miau

--- CatBling/Assets/CasillaSelectScript.cs
+++ CatBling/Assets/CasillaSelectScript.cs
@@ -3,8 +3,21 @@ using UnityEngine;
 public class CasillaSelectScript : MonoBehaviour
 {
     public GamblingManager manager;
+    public SpriteRenderer spriteRenderer;
 
     public Casilla Casilla;
+
+    private void Update()
+    {
+        if (manager.CasillaSeleccionada == Casilla) 
+        {
+            spriteRenderer.enabled = true;
+        }
+        else 
+        {
+            spriteRenderer.enabled = false;
+        }
+    }
 
     public void OnClick()
     {
