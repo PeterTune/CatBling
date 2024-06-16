083e9a233c8c38f4b293ef7a07eb56c5d3a5fe25
JoseMartinCZ <74085376+JoseMartinCZ@users.noreply.github.com>
2024-06-15 20:57:36 -0400

    coins sonido

--- CatBling/Assets/Scripts/ContadorDinero.cs
+++ CatBling/Assets/Scripts/ContadorDinero.cs
@@ -6,11 +6,13 @@ public class CounterTMPro : MonoBehaviour
     public TMP_Text counterText; // Referencia al componente de texto TextMesh Pro en la UI
     private int counter = 1000; // Variable para llevar el conteo
     public VarManager Gato;
+    private AudioSource audioSource; // Referencia al AudioSource
 
     void Start()
     {
         // Inicializa el texto con el valor del contador
         UpdateCounterText();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -22,6 +24,8 @@ public class CounterTMPro : MonoBehaviour
             counter++;
             // Actualiza el texto del contador
             UpdateCounterText();
+
+            PlaySound();
         }
     }
 
@@ -29,5 +33,14 @@ public class CounterTMPro : MonoBehaviour
     {
         // Actualiza el texto del componente de UI con el valor actual del contador
         Gato.dinero = counter;
+    }
+
+    void PlaySound()
+    {
+        // Reproduce el sonido asignado al AudioSource
+        if (audioSource != null)
+        {
+            audioSource.Play();
+        }
     }
 }
